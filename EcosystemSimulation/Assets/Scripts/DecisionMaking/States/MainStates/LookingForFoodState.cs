3362009d3d0a18cc32edccacdcc2d8b1f6826b79
Konrad Walas <kwalas314@gmail.com>
2021-12-27 04:54:39 +0100

    Add better drinking interaction

--- EcosystemSimulation/Assets/Scripts/DecisionMaking/States/MainStates/LookingForFoodState.cs
+++ EcosystemSimulation/Assets/Scripts/DecisionMaking/States/MainStates/LookingForFoodState.cs
@@ -7,7 +7,7 @@ namespace DecisionMaking.States
     {
         public override float CurrentRank => scoreCurve.Evaluate(Needs["Hunger"]);
         
-        private void OnTriggerEnter(Collider other)
+        private void OnTriggerStay(Collider other)
         {
             if(enabled && other.gameObject.CompareTag("Food") && Needs["Hunger"] > 0) //todo abstract this method to State
                 InteractionManager.InteractIfAbleWith(EatingCarrotInteraction, other.gameObject);
