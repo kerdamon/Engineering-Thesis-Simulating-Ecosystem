ce6383afdf14138fa4e3ad13e52dea056f6b16b5
Konrad Walas <kwalas314@gmail.com>
2022-01-01 19:38:49 +0100

    Add life time to metrics

--- EcosystemSimulation/Assets/Scripts/SimulationControl/SimulationController.cs
+++ EcosystemSimulation/Assets/Scripts/SimulationControl/SimulationController.cs
@@ -16,18 +16,26 @@ public class SimulationController : MonoBehaviour
 
     [SerializeField] private Text foxesPopulationText;
     [SerializeField] private Text rabbitsPopulationText;
+    
     [SerializeField] private Text rabbitSpeedMedianText;
     [SerializeField] private Text rabbitSensoryRangeMedianText;
     [SerializeField] private Text rabbitFertilityMedianText;
+    
     [SerializeField] private Text foxSpeedMedianText;
     [SerializeField] private Text foxSensoryRangeMedianText;
     [SerializeField] private Text foxFertilityMedianText;
+    
+    [SerializeField] private Text minRabbitLifeTimeText;
+    [SerializeField] private Text averageRabbitLifeTimeText;
+    [SerializeField] private Text maxRabbitLifeTimeText;
+    
+    [SerializeField] private Text minFoxLifeTimeText;
+    [SerializeField] private Text averageFoxLifeTimeText;
+    [SerializeField] private Text maxFoxLifeTimeText;
 
-    [Range(1, 20)]
-    [SerializeField] private int updatePeriod = 1;
+    [Range(1, 20)] [SerializeField] private int updatePeriod = 1;
 
-    [Range(10, 100)]
-    [SerializeField] private int logPeriod = 20;
+    [Range(10, 100)] [SerializeField] private int logPeriod = 20;
 
     private FileLogger _fileLogger;
 
@@ -38,7 +46,8 @@ public class SimulationController : MonoBehaviour
 
     private void Start()
     {
-        _fileLogger.LogLine("Timestamp,FoxesPopulation,RabbitPopulation,RabbitSpeedMedian,RabbitSensoryRangeMedian,RabbitFertilityMedian,FoxSpeedMedian,FoxSensoryRangeMedian,FoxFertilityMedian");
+        _fileLogger.LogLine(
+            "Timestamp,FoxesPopulation,RabbitPopulation,RabbitSpeedMedian,RabbitSensoryRangeMedian,RabbitFertilityMedian,FoxSpeedMedian,FoxSensoryRangeMedian,FoxFertilityMedian,MinRabbitLifeTime,AverageRabbitLifeTime,MaxRabbitLifeTime,MinFoxLifeTime,AverageFoxLifeTime,MaxFoxLifeTime");
     }
 
     private void Update()
@@ -46,29 +55,47 @@ public class SimulationController : MonoBehaviour
         if (!ShouldLogToFile() && !ShouldUpdateUI()) return;
         var foxesPopulation = CountRabbits();
         var rabbitsPopulation = CountFoxes();
-        var rabbitSpeedMedian = getMedianOfFeatue("Speed", rabbitContainer);
-        var rabbitSensoryRangeMedian = getMedianOfFeatue("SensoryRange", rabbitContainer);
-        var rabbitFertilityMedian = getMedianOfFeatue("Fertility", rabbitContainer);
-        var foxSpeedMedian = getMedianOfFeatue("Speed", foxesContainer);
-        var foxSensoryRangeMedian = getMedianOfFeatue("SensoryRange", foxesContainer);
-        var foxFertilityMedian = getMedianOfFeatue("Fertility", foxesContainer);
+        var rabbitSpeedMedian = GetMedianOfFeature("Speed", rabbitContainer);
+        var rabbitSensoryRangeMedian = GetMedianOfFeature("SensoryRange", rabbitContainer);
+        var rabbitFertilityMedian = GetMedianOfFeature("Fertility", rabbitContainer);
+        var foxSpeedMedian = GetMedianOfFeature("Speed", foxesContainer);
+        var foxSensoryRangeMedian = GetMedianOfFeature("SensoryRange", foxesContainer);
+        var foxFertilityMedian = GetMedianOfFeature("Fertility", foxesContainer);
+        var (minRabbitLifeTime, averageRabbitLifeTime, maxRabbitLifeTime) = GetLifeTimeOfAgents(rabbitContainer);
+        var (minFoxLifeTime, averageFoxLifeTime, maxFoxLifeTime) = GetLifeTimeOfAgents(foxesContainer);
 
         if (ShouldUpdateUI())
         {
             UpdateStatsText(foxesPopulationText, $"Foxes on scene: {foxesPopulation.ToString()}");
             UpdateStatsText(rabbitsPopulationText, $"Rabbits on scene: {rabbitsPopulation.ToString()}");
+            
             UpdateStatsText(rabbitSpeedMedianText,
-                $"Median of rabbit's speed: {rabbitSpeedMedian.ToString(CultureInfo.InvariantCulture)}");
+                $"Rabbit Speed Median: {rabbitSpeedMedian.ToString(CultureInfo.InvariantCulture)}");
             UpdateStatsText(rabbitSensoryRangeMedianText,
-                $"Median of rabbit's speed: {rabbitSensoryRangeMedian.ToString(CultureInfo.InvariantCulture)}");
+                $"Rabbit Sensory Range Median: {rabbitSensoryRangeMedian.ToString(CultureInfo.InvariantCulture)}");
             UpdateStatsText(rabbitFertilityMedianText,
-                $"Median of rabbit's speed: {rabbitFertilityMedian.ToString(CultureInfo.InvariantCulture)}");
+                $"Rabbit Fertility Median: {rabbitFertilityMedian.ToString(CultureInfo.InvariantCulture)}");
+            
             UpdateStatsText(foxSpeedMedianText,
-                $"Median of rabbit's speed: {foxSpeedMedian.ToString(CultureInfo.InvariantCulture)}");
+                $"Fox Speed Median: {foxSpeedMedian.ToString(CultureInfo.InvariantCulture)}");
             UpdateStatsText(foxSensoryRangeMedianText,
-                $"Median of rabbit's speed: {foxSensoryRangeMedian.ToString(CultureInfo.InvariantCulture)}");
+                $"Fox Sensory Range Median: {foxSensoryRangeMedian.ToString(CultureInfo.InvariantCulture)}");
             UpdateStatsText(foxFertilityMedianText,
-                $"Median of rabbit's speed: {foxFertilityMedian.ToString(CultureInfo.InvariantCulture)}");
+                $"Fox Fertility Median: {foxFertilityMedian.ToString(CultureInfo.InvariantCulture)}");
+            
+            UpdateStatsText(minRabbitLifeTimeText,
+                $"Min Rabbit Life Time: {minRabbitLifeTime.ToString(CultureInfo.InvariantCulture)}");
+            UpdateStatsText(averageRabbitLifeTimeText,
+                $"Average Rabbit Life Time: {averageRabbitLifeTime.ToString(CultureInfo.InvariantCulture)}");
+            UpdateStatsText(maxRabbitLifeTimeText,
+                $"Max Rabbit Life Time: {maxRabbitLifeTime.ToString(CultureInfo.InvariantCulture)}");
+            
+            UpdateStatsText(minFoxLifeTimeText,
+                $"Min Fox Life Time: {minFoxLifeTime.ToString(CultureInfo.InvariantCulture)}");
+            UpdateStatsText(averageFoxLifeTimeText,
+                $"Average Fox Life Time: {averageFoxLifeTime.ToString(CultureInfo.InvariantCulture)}");
+            UpdateStatsText(maxFoxLifeTimeText,
+                $"Max Fox Life Time Text: {maxFoxLifeTime.ToString(CultureInfo.InvariantCulture)}");
         }
 
         if (ShouldLogToFile())
@@ -76,12 +103,22 @@ public class SimulationController : MonoBehaviour
             _fileLogger.LogLine($"{Academy.Instance.StepCount.ToString()}" +
                                 $"{foxesPopulation.ToString()}," +
                                 $"{rabbitsPopulation.ToString()}," +
+                                
                                 $"{rabbitSpeedMedian.ToString(CultureInfo.InvariantCulture)}," +
                                 $"{rabbitSensoryRangeMedian.ToString(CultureInfo.InvariantCulture)}," +
                                 $"{rabbitFertilityMedian.ToString(CultureInfo.InvariantCulture)}," +
+                                
                                 $"{foxSpeedMedian.ToString(CultureInfo.InvariantCulture)}," +
                                 $"{foxSensoryRangeMedian.ToString(CultureInfo.InvariantCulture)}," +
-                                $"{foxFertilityMedian.ToString(CultureInfo.InvariantCulture)}");
+                                $"{foxFertilityMedian.ToString(CultureInfo.InvariantCulture)}," +
+                                
+                                $"{minRabbitLifeTime.ToString(CultureInfo.InvariantCulture)}," +
+                                $"{averageRabbitLifeTime.ToString(CultureInfo.InvariantCulture)}," +
+                                $"{maxRabbitLifeTime.ToString(CultureInfo.InvariantCulture)}," +
+                                
+                                $"{minFoxLifeTime.ToString(CultureInfo.InvariantCulture)}," +
+                                $"{averageFoxLifeTime.ToString(CultureInfo.InvariantCulture)}," +
+                                $"{maxFoxLifeTime.ToString(CultureInfo.InvariantCulture)}");
         }
     }
 
@@ -95,7 +132,7 @@ public class SimulationController : MonoBehaviour
         return Time.frameCount % updatePeriod == 0;
     }
 
-    private void UpdateStatsText(Text textElement, string text)
+    private static void UpdateStatsText(Text textElement, string text)
     {
         textElement.text = text;
     }
@@ -109,23 +146,35 @@ public class SimulationController : MonoBehaviour
     {
         return CountAgents(rabbitContainer);
     }
-    
+
     private static int CountAgents(Transform container)
     {
         return container.childCount;
     }
 
-    private float getMedianOfFeatue(string featureName, Transform agentsContainer)
+    private (int minLifeTime, float averageLifeTime, int maxLifeTime) GetLifeTimeOfAgents(Transform container)
+    {
+        var lifeTimes = new List<int>();
+        foreach (Transform agent in container)
+        {
+            lifeTimes.Add(agent.GetComponent<MovementAgent>().LifeTime);
+        }
+
+        return (lifeTimes.Min(), (float) lifeTimes.Average(), lifeTimes.Max());
+    }
+
+    private float GetMedianOfFeature(string featureName, Transform agentsContainer)
     {
         var features = new List<int>();
         foreach (Transform agent in agentsContainer)
         {
             features.Add(agent.GetComponent<Features>()[featureName]);
         }
+
         return GetMedianFromList(features);
     }
 
-    private float GetMedianFromList(List<int> list)
+    private static float GetMedianFromList(IReadOnlyCollection<int> list)
     {
         var count = list.Count;
         var halfIndex = count / 2;
