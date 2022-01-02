b28059333caf8446fdec63fbed24a11ae078cc37
Konrad Walas <kwalas314@gmail.com>
2022-01-02 16:35:41 +0100

    Add training eating rabbit interaction

--- EcosystemSimulation/Assets/Scripts/Interaction/InteractionManagers/RabbitInteractionManager.cs
+++ EcosystemSimulation/Assets/Scripts/Interaction/InteractionManagers/RabbitInteractionManager.cs
@@ -9,19 +9,8 @@ namespace Interaction.InteractionManagers
     {
         private EatingCarrotInteraction _eatingCarrotInteraction;
 
-        private TrainingArea _trainingArea; //TODO change; only to use method to randomize position of this agent upon eating in training mode, it is probably spaghetti code
-
-        private float rabbit_on_eaten;  //todo change name and add reward at the end
-
-        private bool _isTraining;    //TODO check; indicates if agent is currently trained or not (it is used for reseting agent in training scenarios on eaten)
-
         protected override void Start()
         {
-            _trainingArea = GetComponentInParent<TrainingArea>();
-            _isTraining = Mathf.Abs(Academy.Instance.EnvironmentParameters.GetWithDefault("is_training", 0.0f)) > 0.0001f;
-
-            rabbit_on_eaten = Academy.Instance.EnvironmentParameters.GetWithDefault("rabbit_on_eaten", 0.0f);
-
             var rabbit_eating_carrot_reward = Academy.Instance.EnvironmentParameters.GetWithDefault("rabbit_eating_carrot_reward", 0.0f);
             _eatingCarrotInteraction = GetComponent<EatingCarrotInteraction>();
             AddRewardAfterInteraction(_eatingCarrotInteraction, rabbit_eating_carrot_reward);
@@ -33,15 +22,7 @@ namespace Interaction.InteractionManagers
         public float OnEaten()
         {
             var parent = transform.parent;
-            MovementAgent.AddReward(rabbit_on_eaten);
-            if (_isTraining)
-            {
-                _trainingArea.RandomizePositionAndRotationWithCollisionCheck(parent, parent.parent);
-            }
-            else
-            {
-                Destroy(parent.gameObject);
-            }
+            Destroy(parent.gameObject);
             return 50.0f;   //todo make this depenndent on feature like size
         }
     }
