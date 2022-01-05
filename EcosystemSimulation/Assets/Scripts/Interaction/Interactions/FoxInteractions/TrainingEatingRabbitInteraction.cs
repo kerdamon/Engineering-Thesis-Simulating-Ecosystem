251091f0c846cdc67ce7a27e662cb5c8e73a0f54
Konrad Walas <kwalas314@gmail.com>
2022-01-05 03:01:58 +0100

    Add penlaty for rabbit upon being eaten by fox

--- EcosystemSimulation/Assets/Scripts/Interaction/Interactions/FoxInteractions/TrainingEatingRabbitInteraction.cs
+++ EcosystemSimulation/Assets/Scripts/Interaction/Interactions/FoxInteractions/TrainingEatingRabbitInteraction.cs
@@ -1,4 +1,5 @@
 ﻿using Interaction.InteractionManagers;
+using Unity.MLAgents;
 using UnityEngine;
 
 namespace Interaction.FoxInteractions
@@ -7,11 +8,23 @@ namespace Interaction.FoxInteractions
     {
         [SerializeField] private TrainingArea trainingArea;
 
+        private float rabbit_on_eaten;
+        
+        protected override void Start()
+        {
+            base.Start();
+            rabbit_on_eaten = Academy.Instance.EnvironmentParameters.GetWithDefault("rabbit_on_eaten", 0.0f);
+        }
+
         protected override void AtInteractionEnd()
         {
             var rabbit = SecondSimulationObject.transform;
             var rabbitContainer = rabbit.parent;
             trainingArea.RandomizePositionAndRotationWithCollisionCheck(rabbit, rabbitContainer);
+
+            if (Mathf.Abs(rabbit_on_eaten) > 0.00001f) return;
+            rabbit.GetComponent<MovementAgent>().AddReward(rabbit_on_eaten);
+            Debug.Log($"Added reward of value {rabbit_on_eaten} after rabbit was eaten", this);
         }
     }
 }
