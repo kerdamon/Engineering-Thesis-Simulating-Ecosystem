08eab92544e2444333a2149860e02079b5a00263
Konrad Walas <kwalas314@gmail.com>
2022-01-08 16:17:35 +0100

    Fix interrupting interaction

--- EcosystemSimulation/Assets/Scripts/Interaction/InteractionManagers/RabbitInteractionManager.cs
+++ EcosystemSimulation/Assets/Scripts/Interaction/InteractionManagers/RabbitInteractionManager.cs
@@ -1,20 +1,30 @@
-﻿using Interaction;
+﻿using DecisionMaking;
+using DecisionMaking.States;
+using DecisionMaking.States.EventStates;
+using Interaction;
 using Interaction.RabbitInteractions;
 using Unity.MLAgents;
 using UnityEngine;
 
 namespace Interaction.InteractionManagers
 {
-    public class RabbitInteractionManager : InteractionManager 
+    public class RabbitInteractionManager : InteractionManager
     {
         private EatingCarrotInteraction _eatingCarrotInteraction;
+        private StateMachine _stateMachine;
+        private EscapingPredatorState _escapingPredatorState;
 
         protected override void Start()
         {
-            var rabbit_eating_carrot_reward = Academy.Instance.EnvironmentParameters.GetWithDefault("rabbit_eating_carrot_reward", 0.0f);
+            var rabbit_eating_carrot_reward =
+                Academy.Instance.EnvironmentParameters.GetWithDefault("rabbit_eating_carrot_reward", 0.0f);
             _eatingCarrotInteraction = GetComponent<EatingCarrotInteraction>();
             AddRewardAfterInteraction(_eatingCarrotInteraction, rabbit_eating_carrot_reward);
             RegisterUpdatingCurrentInteractionAfterEndOf(_eatingCarrotInteraction);
+
+            var parent = transform.parent;
+            _stateMachine = parent.GetComponentInChildren<StateMachine>();
+            _escapingPredatorState = parent.GetComponentInChildren<EscapingPredatorState>();
 
             base.Start();
         }
@@ -23,7 +33,14 @@ namespace Interaction.InteractionManagers
         {
             var parent = transform.parent;
             Destroy(parent.gameObject);
-            return 50.0f;   //todo make this depenndent on feature like size
+            return 50.0f; //todo make this depenndent on feature like size
+        }
+
+        protected override void StopInteractionWhenAgentDontWantTo()
+        {
+            if (IsInteracting && _stateMachine.CurrentState == _escapingPredatorState)
+                CurrentInteraction.Interrupt();
+            base.StopInteractionWhenAgentDontWantTo();
         }
     }
 }
